02003353893f1913bf27f060e40539991c927474
Anton <anton.shcherbatykh.2000@gmail.com>
2026-01-29 03:54:22 +0300

    Update script_app.sh

--- script_app.sh
+++ script_app.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-    sudo apt install -y docker docker-compose-v2
+cd /opt
 
-if [ ! -d "/opt/shvirtd-example-python" ] ; then
-    sudo git clone https://github.com/Anton-Shcherbatykh/FOPS-38_15-05 /opt/FOPS-38_15-05
-else
-    cd /opt/FOPS-38_15-05
-    sudo git pull
-fi
+git clone https://github.com/Anton-Shcherbatykh/FOPS-38_15-05.git
 
-pwd
+cd FOPS-38_15-05
 
-sudo docker compose -f compose.yaml up
+docker compose up --build -d
